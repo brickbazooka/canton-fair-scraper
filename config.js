e2ce6374b62bb6f1eff6a6ab8fcc247b2c169257
Brick <brick.berserker@gmail.com>
2025-03-06 18:10:27 +0530

    Add all other main category ids to config.js (as comments)

--- config.js
+++ config.js
@@ -2,10 +2,17 @@
 const CATEGORIES_TO_SCRAPE = [
 	'461147245295706112', // Electronics & Appliance
 	'461147369757478912', // Manufacturing
+	// '461147262152626176', // Vehicles & Two Wheels
 	'461148003609088000', // Light & Electrical
 	'461147703003324416', // Hardware
-	'461147098763513856', // Health & Recreation
+	// '461147108741746688', // Housewares
+	// '461147061383880704', // Gifts & Decorations
+	// '461147101884076032', // Building & Furniture
+	// '461147079700406272', // Toys & Children Baby and Maternity
+	// '461147105440849920', // Fashion
+	// '461147103742152704', // Home Textiles
 	'461147104627134464', // Stationery
+	'461147098763513856', // Health & Recreation
 ];
 
 export default {
